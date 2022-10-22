dfbd7f5f3505bbb3ef32bb70f5ee612c97d938db
Pfefan <stefanpfeiffer04@gmail.com>
2022-10-22 19:57:40 +0200

    GUI changes, no duplicate files

---
+++ download.py
@@ -0,0 +1,47 @@
+import os
+from threading import Thread
+
+from pytube import Playlist
+
+
+class down():
+    def __init__(self, _GUI, savepath_) -> None:
+        self.GUI = _GUI
+        self.path = savepath_
+        self.invalid_characters = '\/:*?"<>|'
+
+    def main(self):
+        if not os.path.exists("playlists.txt"):
+            open("playlists.txt", "w+").close()
+        self.download()
+
+    def download(self):
+        """Downloads a given songs in a List of Playlists"""
+        urls = []
+        with open("playlists.txt", "r", encoding="utf8") as pfile:
+            urls = pfile.readlines()
+            pfile.close()
+
+        for url in urls:
+            counter = 1
+            p = Playlist(url)
+            #multiplier = 100/len(p.videos)
+            self.GUI.pbar.setMaximum(len(p.videos))
+            if not os.path.exists(os.path.join(self.path, p.title)):       # creates playlist dir if it doesnt exist
+                os.mkdir(os.path.join(self.path, p.title))
+            files = os.listdir(os.path.join(self.path, p.title))
+
+            self.GUI.pbarlabel.setText(f'Syncing: {p.title}')
+            for video in p.videos:
+                self.GUI.pbarlabel.setText(f'Syncing: {p.title} {counter}/{len(p.videos)}')
+                self.GUI.pbar.setValue(counter)
+
+                for char in self.invalid_characters: video.title =  video.title.replace(char, "") # refactore video title so no files will be save with invalid strings
+
+                file = video.title + ".mp3"
+                if file not in files:       #checks if video is already download so it doesnt have to repeat it
+                    audio = video.streams.filter(only_audio=True).first()
+                    out_file = audio.download(output_path=os.path.join(self.path, p.title))
+                    new_file = os.path.join(self.path, p.title, file)
+                    os.rename(out_file, new_file)
+                counter += 1
--- main.py
+++ main.py
@@ -1,45 +1,72 @@
+import json
+import os
 import sys
+from threading import Thread
 
+from PyQt6 import QtGui
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
-from aud_down import aud_down
+
+from download import down
 
 
 class MainWindow(QWidget):
     def __init__(self) -> None:
         super().__init__()
-
         self.initUI()
+        self.config = {}
+        if not os.path.exists("config.conf"):
+            config = {
+                "savepath" : f"{os.getcwd()}\\download"
+            }
+            with open("config.conf", "w+") as file:
+                json.dump(config, file)
+
+        with open("config.conf", "r") as file:
+            self.config = json.load(file)
 
     def initUI(self):
         grid = QGridLayout()
         self.setLayout(grid)
+        titlelable = QLabel("YouTube Playlist Sync by Pfefan")
+        self.pbarlabel = QLabel("Progress...")
+        self.pbar = QProgressBar()
+        playlist_input = QLineEdit()
+        seldir_btn = QPushButton("Select save directory")
+        add_btn = QPushButton("Add Playlist")
+        sync_btn = QPushButton("Sync playlist")
 
-        sync_btn = QPushButton("Download / Sync playlist")
-        sync_btn.setMaximumHeight(50)
-        sync_btn.setMaximumWidth(150)
+        self.pbarlabel.setMinimumWidth(200)
+        seldir_btn.clicked.connect(self.pick_dir)
+
         sync_btn.clicked.connect(self.syncbtn_click)
+        sync_btn.setMaximumWidth(100)
+        sync_btn.setMaximumHeight(40)
 
-        inputbox = QLineEdit()
-        inputbox.setMaximumWidth(300)
+        grid.addWidget(titlelable, 0, 0)
 
-        addplaylist_btn = QPushButton("Add Playlist")
-        addplaylist_btn.setMaximumHeight(50)
-        addplaylist_btn.setMaximumWidth(100)
+        grid.addWidget(playlist_input, 1, 0)
+        grid.addWidget(add_btn, 1, 1)
 
+        grid.addWidget(seldir_btn, 2, 1)
 
-        grid.addWidget(sync_btn)
-        grid.addWidget(inputbox)
-        grid.addChildWidget(addplaylist_btn)
+        grid.addWidget(sync_btn, 3, 0, 3, 1)
 
-        self.resize(640, 360)
+        grid.addWidget(self.pbar, 4, 0, 4 ,1)
+        grid.addWidget(self.pbarlabel, 4, 1, 4 ,1)
+
+        self.resize(480, 270)
         self.setWindowTitle('Yt-Playlist-downloader')
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
         self.show()
 
-    def syncbtn_click(self):
-        aud_down().main()
+    def pick_dir(self):
+        self.savepath = QFileDialog.getExistingDirectory(self, "Select Folder")
 
+    def syncbtn_click(self):
+        Thread(target=down(self, self.config["savepath"]).main, daemon=True).start()
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
