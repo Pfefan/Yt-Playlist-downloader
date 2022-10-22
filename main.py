2eaa6d6aafc1ed60c21b12fb015521ee573a9cc5
Pfefan <stefanpfeiffer04@gmail.com>
2022-10-22 11:41:12 +0200

    gui changes

--- main.py
+++ main.py
@@ -21,7 +21,17 @@ class MainWindow(QWidget):
         sync_btn.setMaximumWidth(150)
         sync_btn.clicked.connect(self.syncbtn_click)
 
+        inputbox = QLineEdit()
+        inputbox.setMaximumWidth(300)
+
+        addplaylist_btn = QPushButton("Add Playlist")
+        addplaylist_btn.setMaximumHeight(50)
+        addplaylist_btn.setMaximumWidth(100)
+
+
         grid.addWidget(sync_btn)
+        grid.addWidget(inputbox)
+        grid.addChildWidget(addplaylist_btn)
 
         self.resize(640, 360)
         self.setWindowTitle('Yt-Playlist-downloader')
